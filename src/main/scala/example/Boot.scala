15b95efbc16b9ae4f8db9141835bb82c430fb322
Igo Brilhante <brilhanteigo@gmail.com>
2020-04-18 10:46:12 -0300

    Added bloop and metals

--- src/main/scala/example/Boot.scala
+++ src/main/scala/example/Boot.scala
@@ -6,20 +6,19 @@ import example.infrastructure._
 import example.interop.slick.dbio._
 import slick.driver.H2Driver.api._
 import scala.io.StdIn
-import zio.{ Runtime, ZIO }
+import zio.{Runtime, ZIO}
 import example.infrastructure.tables.AssetsTable
 import example.infrastructure.tables.PortfolioAssetsTable
 
 object Boot extends App {
 
-  
   implicit val system = ActorSystem(name = "zio-example-system")
   implicit val ec = system.dispatcher
 
-  class LiveEnv 
-    extends SlickAssetRepository 
-    with SlickPortfolioAssetRepository 
-    with LiveDatabaseProvider
+  class LiveEnv
+      extends SlickAssetRepository
+      with SlickPortfolioAssetRepository
+      with LiveDatabaseProvider
 
   val liveEnv = new LiveEnv
   val assets = TableQuery[AssetsTable.Assets]
@@ -39,13 +38,12 @@ object Boot extends App {
 
   val setupIO = ZIO.fromDBIO(setup).provide(liveEnv)
   Runtime.default.unsafeRun(setupIO)
- 
 
   val bindingFuture = Http().bindAndHandle(api.route, host, port)
 
   println(s"Server online at http://localhost:8080/\nPress RETURN to stop...")
-    StdIn.readLine()
-    bindingFuture
-      .flatMap(_.unbind())
-      .onComplete(_ => system.terminate())
+  StdIn.readLine()
+  bindingFuture
+    .flatMap(_.unbind())
+    .onComplete(_ => system.terminate())
 }
